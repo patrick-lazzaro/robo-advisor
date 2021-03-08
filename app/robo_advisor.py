8f06d25e1e21d0d9550af91e779aaf468ef6bc65
patrick-lazzaro <77748643+patrick-lazzaro@users.noreply.github.com>
2021-03-07 20:13:18 -0500

    Request and Parsed Response

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,27 @@
 # this is the "app/robo_advisor.py" file
 
+import requests
+import json
+
+#
+# INFO INPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) #> <class 'requests.models.Response'>
+# print(response.status_code) #> 200
+# print(response.text)
+
+parsed_response = json.loads(response.text)
+
+breakpoint()
+
+#
+# INFO OUTPUTS
+#
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -16,3 +38,6 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+ 
