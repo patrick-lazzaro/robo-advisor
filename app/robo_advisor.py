06fd43a7089253f72f034625f8295c37ab316a4a
patrick-lazzaro <77748643+patrick-lazzaro@users.noreply.github.com>
2021-03-07 22:37:47 -0500

    Dynamic Latest Close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -33,7 +33,13 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close = parsed_response["Time Series (Daily)"]["2021-03-05"]["4. close"]
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys()) # TODO: assumes first day is on top, sort to ensure latest day is first
+
+latest_day = dates[0]
+latest_close = tsd[latest_day]["4. close"]
 
 # breakpoint()
 
